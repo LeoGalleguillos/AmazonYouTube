b9e500db2a5e4c08b4389610654443aba6e70591
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-02-12 01:11:21 +0000

    Add unique index on `active`, `name` columns

--- sql/test/browse_node/create.sql
+++ sql/test/browse_node/create.sql
@@ -3,5 +3,6 @@ CREATE TABLE `browse_node` (
   `name` VARCHAR(255) NOT NULL,
   `active` tinyint(1) unsigned not null default 1,
   PRIMARY KEY (`browse_node_id`),
-  UNIQUE (`name`)
+  UNIQUE `name` (`name`),
+  UNIQUE `active_name` (`active`, `name`)
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;

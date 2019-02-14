9c1e2d27d6e485ca9a07fee9043fd88f5933c690
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-02-14 17:44:54 +0000

    Write sql method to select browse node name and count for videos not yet uploaded

--- src/Model/Table/BrowseNode.php
+++ src/Model/Table/BrowseNode.php
@@ -1,6 +1,7 @@
 <?php
 namespace LeoGalleguillos\AmazonYouTube\Model\Table;
 
+use Generator;
 use Zend\Db\Adapter\Adapter;
 
 class BrowseNode
@@ -37,5 +38,39 @@ class BrowseNode
                     ->query($sql)
                     ->execute($parameters)
                     ->getGeneratedValue();
+    }
+
+    public function selectBrowseNodeNameCount(): Generator
+    {
+        $sql = '
+            select amazon_you_tube.browse_node.name
+                 , count(*) as `count`
+              from amazon_you_tube.browse_node
+
+              join amazon.browse_node
+             using (name)
+
+              join amazon.browse_node_product
+                on amazon.browse_node_product.browse_node_id = amazon.browse_node.browse_node_id
+
+              join amazon.product_video
+             using (product_id)
+
+              left
+              join amazon_you_tube.product_video_upload_log
+             using (product_video_id)
+
+             where amazon_you_tube.product_video_upload_log.created is null
+
+             group
+                by amazon_you_tube.browse_node.name
+
+             order
+                by `count` desc
+                 ;
+        ';
+        foreach ($this->adapter->query($sql)->execute() as $array) {
+            yield $array;
+        }
     }
 }
